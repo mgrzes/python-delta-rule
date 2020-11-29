702862af180b290056b796ad947a9065c6cfab0a
Marek Grzes <m.grzes@kent.ac.uk>
2020-11-29 17:31:21 +0000

    compile the notebook in the terminal

--- compile.sh
+++ compile.sh
@@ -8,3 +8,6 @@ jupyter nbconvert --to html delta-rule-exploration.ipynb
 jupytext --to py delta-rule-exploration.ipynb
 
 scp delta-rule-exploration.py $1:/courses/co636/delta_rule/
+
+# Replace the existing notebook with the new output
+jupyter nbconvert --execute --inplace delta-rule-exploration.ipynb
