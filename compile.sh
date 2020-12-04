caf6cca9ff4e698d9c9c7242a3cc963d030a6149
Marek Grzes <m.grzes@kent.ac.uk>
2020-12-04 12:00:47 +0000

    conversion to python fixed

--- compile.sh
+++ compile.sh
@@ -5,7 +5,7 @@
 jupyter nbconvert --to html delta-rule-exploration.ipynb
 
 # convert to python with comments
-jupytext --to py delta-rule-exploration.ipynb
+jupyter nbconvert --to python delta-rule-exploration.ipynb
 
 scp delta-rule-exploration.py $1:/courses/co636/delta_rule/
 
