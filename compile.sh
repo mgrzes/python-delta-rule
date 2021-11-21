c29480d9c2b3be48c2187d250a2e4752be62e87e
Marek Grzes <m.grzes@kent.ac.uk>
2021-11-21 11:31:10 +0000

    a few clarifications added; no Python code changed in this revision

--- compile.sh
+++ compile.sh
@@ -5,9 +5,10 @@
 jupyter nbconvert --to html delta-rule-exploration.ipynb
 
 # convert to python with comments
-jupyter nbconvert --to python delta-rule-exploration.ipynb
+# jupyter nbconvert --to python delta-rule-exploration.ipynb
 
-scp delta-rule-exploration.py $1:/courses/co636/delta_rule/
+scp delta-rule-exploration.html $1:/courses/co636/wk11_delta_rule/
 
 # Replace the existing notebook with the new output
-jupyter nbconvert --execute --inplace delta-rule-exploration.ipynb
+# jupyter nbconvert --execute --inplace delta-rule-exploration.ipynb
+
